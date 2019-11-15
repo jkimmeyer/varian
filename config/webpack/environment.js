bc9711d58e5c2e2954e823b3f8954873ac9ef5e5
Joe Pantazidis <joe.pantazidis@outlook.de>
2019-11-15 21:08:44 +0200

    add jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,15 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+
 module.exports = environment
