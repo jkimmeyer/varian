2c0cdb26aa8cab4934cc81ee244f9b6aa39ac28b
jkimmeyer <jkimmeyer@googlemail.com>
2019-11-16 00:20:30 +0100

    add patient overview

--- app/javascript/instances/default-view.vue
+++ app/javascript/instances/default-view.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="app">
+    <the-header></the-header>
+    <patient-overview></patient-overview>
     <b-button type="is-primary">{{ message }}</b-button>
   </div>
 </template>
 
 <script>
+import TheHeader from '../components/header';
+import PatientOverview from '../components/patient-overview';
+
 export default {
+  components: {
+    TheHeader,
+    PatientOverview,
+  },
   data: function () {
     return {
       message: "Hello Vue!"
