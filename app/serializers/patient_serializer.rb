f3f0b8109cd8dfcf37bb2d5365882f8c1e99d831
Joe Pantazidis <joe.pantazidis@outlook.de>
2019-11-16 20:16:09 +0200

    add treatment_plans to patient serializer

--- app/serializers/patient_serializer.rb
+++ app/serializers/patient_serializer.rb
@@ -1,7 +1,17 @@
 class PatientSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :status
+  attributes :id, :first_name, :last_name, :status, :treatment_plans
 
   def status
     object.current_treatment_plan.status
+  end
+
+  def treatment_plans
+    object.treatment_plans.pluck(:id, :name, :status).map do |plan|
+      {
+        id: plan.first,
+        name: plan.second,
+        status: plan.last,
+      }
+    end
   end
 end
